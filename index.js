023255beb952800af7885a224dca7730bb312b74
Cameron Small <csmall@aegistg.com>
2023-07-26 21:32:37 -0500

    First commit to my Integrating With HubSpot I: Foundations practicum repository.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please include the private app access token in your repo BUT only an access token built in a TEST ACCOUNT. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = 'pat-na1-734c46cf-85a1-45e9-8473-39c2eb3178fa';
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
