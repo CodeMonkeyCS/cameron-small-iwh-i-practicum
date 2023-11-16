b6c7fbc37d59ceba9ca7140948e19721551aa6e6
Cameron Small <csmall@aegistg.com>
2023-11-15 22:14:21 -0600

    Working on getting the post and patch working.

--- index.js
+++ index.js
@@ -12,20 +12,15 @@ const PRIVATE_APP_ACCESS = 'pat-na1-734c46cf-85a1-45e9-8473-39c2eb3178fa';
 const CUSTOM_SCHEMAS_URL = `https://api.hubspot.com/crm/v3/schemas`;
 const CUSTOM_OBJECTS_URL = 'https://api.hubapi.com/crm/v3/objects';
 
+let petObjectTypeID = '';
 let petObjects = []
 
 /// \brief find and return the first instance of the Pet object schema
 function findPetSchema(arrayOfObjects) {
-    let petIndex = -1;
-    for (let index = 0; index < arrayOfObjects.length; ++index) {
-        if (arrayOfObjects[index]['labels']['singular'] === "Pet") {
-            petIndex = index;
-            break;
-        }
-    }
+    // find the Pet object schema
+    const petObject = arrayOfObjects.find((element) => { return element['labels']['singular'] === "Pet" });
 
     // create a schema of the data we care about
-    const petObject = arrayOfObjects[petIndex];
     const petSchema = {
         objectTypeId : petObject['objectTypeId'],
         fullyQualifiedName : petObject['fullyQualifiedName'],
@@ -49,8 +44,9 @@ app.get('/', async (req, res) => {
         const response = await axios.get(customObjects, { headers });
         const petSchema = findPetSchema(response.data.results);
 
-        const petProperties = '&properties=pet_name&properties=pet_bio&properties=job_title'
-        const petsURL = `${CUSTOM_OBJECTS_URL}/${petSchema.objectTypeId}?${petProperties}`;
+        petObjectTypeID = petSchema.objectTypeId;
+        const petProperties = 'properties=pet_name&properties=pet_bio&properties=job_title'
+        const petsURL = `${CUSTOM_OBJECTS_URL}/${petObjectTypeID}?${petProperties}`;
         const petsResponse = await axios.get(petsURL, { headers });
 
         petObjects = petsResponse.data.results;
@@ -77,20 +73,58 @@ app.get('/update', async (req, res) => {
 
 // * Code for Route 3 goes here
 app.post('/update', async (req, res) => {
-    // const update = {
-    //     properties: {
-    //         "pet_name": req.body.pet_name
-    //         "pet_bio": req.body.pet_bio
-    //         "job_title": req.body.job_title
-    //     }
-    // }
-    //
-    // const email = req.query.email;
-    // const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    // const headers = {
-    //     Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-    //     'Content-Type': 'application/json'
-    // };
+
+    const petsURL = `${CUSTOM_OBJECTS_URL}/${petObjectTypeID}`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    // POST
+    console.log(req.body.pet_name.length);
+    if (req.body.pet_name.length > petObjects.length && req.body.pet_name[req.body.pet_name.length - 1] != '') {
+        // POST
+
+        const create = {properties: []}
+        for (let i = 0; i < req.body.pet_name.length; i++) {
+            create.properties.push({
+                "pet_name": req.body.pet_name[i],
+                "pet_bio": req.body.pet_bio[i],
+                "job_title": req.body.job_title[i],
+            });
+        }
+
+        try {
+            const petsURL = `${CUSTOM_OBJECTS_URL}/${petObjectTypeID}`;
+            await axios.post(petsURL, create, { headers });
+        }
+        catch (error) {
+            console.error(error);
+        }
+
+    }
+    else {
+        // PATCH
+
+        const update = {properties: []}
+        for (let i = 0; i < petObjects.length; i++) {
+            update.properties.push({
+                "pet_name": req.body.pet_name[i],
+                "pet_bio": req.body.pet_bio[i],
+                "job_title": req.body.job_title[i],
+            });
+        }
+
+        console.log(update);
+
+        try {
+            await axios.patch(petsURL, update, { headers });
+        }
+        catch (error) {
+            console.error(error);
+        }
+
+    }
 
     try {
         // await axios.patch(updateContact, update, { headers } );
